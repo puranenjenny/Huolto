79604e525406c345508338d03cbac2ca0c36b016
AnttiK87 <123761089+AnttiK87@users.noreply.github.com>
2023-04-26 23:06:03 +0300

    Add files via upload

---
+++ Nettisivut/js/header.js
@@ -0,0 +1,25 @@
+/* js alasvetovalikkoa varten */
+//Näyttää valikon lisäämällä luokan
+function showDropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+
+  }
+  
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+    
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
+
+
+
+
+
