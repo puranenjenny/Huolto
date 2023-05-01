0a4a2bc19d532e35f3e947da871c56a30e9a54d4
AnttiK87 <Antti.Kortelainen@edu.lapinamk.fi>
2023-05-01 18:36:22 +0300

    Lisäsin sisällön tälle sivulle

--- Nettisivut/palvelut.php
+++ Nettisivut/palvelut.php
@@ -1,8 +1,73 @@
 
 <?php include 'header.php';?>
 
+<!--tästä alkaa sivun kontentti-->
+<div class="container2">
 
+    <div class="row text-center d-flex justify-content-center  mx-0">
+        <div class="col-12 col-md-8"><h3>Tarjoamme kiinteistönhoitopalveluita laidasta laitaan niin taloyhtiöille, omakotiasujille
+        kuin yrityksien liiketiloillekin.</h3></div>
+    </div>
 
+    <div class="row vali mx-0"></div>
 
+    <div class="row text-center d-flex justify-content-center mx-0">
+        <div class="col-12 col-md-8"><p><b>R.Autio Oy sitoutuu tarjoamaan kiinteistöjen hoitopalvelut ammattitaidolla, laadukkaasti ja asiakkaiden toiveiden mukaisesti. 
+        Hoidamme laaja-alaisesti kiinteistöjen ylläpitämiseen, huoltoon ja siivoukseen liittyvät tehtävät vuosikymmenien kokemuksella. Olemme tulleet asiakkaillemme 
+        tutuiksi taloyhtiöiden asuinkiinteistöjen vankkumattomina huolehtijoina. Lisäksi kauttamme onnistuvat myös räätälöidyt palvelut asiakkaan tarpeiden mukaisesti myös pienempien 
+        asuinkohteiden huoltotöihin kuin liiketilojen ylläpitotarpeisiin. Tavoitteenamme on olla asiakkaillemme luotettava ja helposti lähestyttävä huoltopalveluidentarjoaja.</b></p></div>
+    </div>
+    <div class="row vali mx-0"></div>
+    <div class="row vali mx-0"></div>
+
+   
+    <div class="row tyontekijat text-center mx-0">
+        <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/repair2.png" class="img-fluid palvelut1 rounded-circle" alt="palvelut1"><br>
+        <p><br><b>Kiinteistönhuolto</b><br>
+            Yleiset ja ylläpitävät tehtävät, kuten kiinteistön kunnon jatkuva tarkkailu ja talotekniikan toiminnan seuranta. 
+            Huolehdimme kiinteistöstäsi kokonaisvaltaisesti kausivaihtelut huomioivan suunnitelman mukaisesti, tietenkään 
+            unohtamatta auto- ja roskakatoksia sekä muita yleisiätiloja.<br><br>
+            </p></div>
+        <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki12.jpg" class="img-fluid palvelut2 rounded-circle" alt="palvelut2">
+        <p><br><b>Lumityöt</b><br>
+            Lumityöt hoituvat ketterästi uudella ja ympäristöystävällisellä kalustolla, joka on suunniteltu erityisesti ahtaita pihoja silmälläpitäen.
+            Hoidamme myös liukkauden torjunnan, kattojen lumikuorman tarkkailun sekä tarvittaessa pihoihin kertyneiden aurauslumien poiskuljettamisen.<br><br>
+            </p></div>
+        <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki6.jpg" class="img-fluid palvelut3 rounded-circle" alt="palvelut3">
+        <p><br><b>Viheralueiden hoito</b><br>
+            Tarjoamme puutarhuritasoista viheralueiden hoitoa. Huolehdimme niin piha-alueiden pensaista, puista, nurmikon leikkuusta, kulkuväylien siisteydestä 
+            kuin syksyisestä lehtien haravoimisestakin. Tarvittaessa kauttamme hoituvat myös pensasaitojen leikkaukset sekä viheralueiden istutustyöt.<br><br>
+            </p></div>
+        <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki11.jpg" class="img-fluid palvelut4 rounded-circle" alt="palvelut4">
+        <p><br><b>Siivuostyöt</b><br>
+            Huolehdimme siisteydestä tehokkaasti laadukkailla välineillä ja puhdistusaineilla asuinkiinteistöjen yleisissätiloissa sekä yritysten toimitiloissa. 
+            Räätälöimme siivoispalvelut juuri teidän tarpeidenne ja toiveidenne mukaisesti. Hoidamme tarvittaessa myös ikkunoiden pesut sekä kertaluontoiset siivoukset 
+            kuten asuntojen tyhjennykset.<br><br>
+            </p></div>
+        <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/repair1.jpg" class="img-fluid palvelut5 rounded-circle" alt="palvelut5">
+        <p><br><b>Remontit</b><br>
+            Kauttamme onnistuvat myös yleisten- ja asuintilojenkin remontointi. Hoidamme niin pienet maalia ja tapettia kaipaavat pintaremontit kuin 
+            suuremmat kaikenkattavat saneeraukset. Rakennusmiehemme ovat alansa ammattilaisia ja omaavat tarvittavat sertifikaatit laadukkaaseen ja luotettavaan korjaustöiden suorittamiseen.<br><br>
+            </p></div>
+        <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki5.jpg" class="img-fluid palvelut6 rounded-circle" alt="palvelut6">
+        <p><br><b>24h päivystys</b><br>
+            Vahingon sattuessa apu on lähellä. Huoltomiehemme päivystää kellonympäri vuoden jokaisena päivänä. Nyt verkkosivuillamme saatavilla on myös kellonajasta riippumaton 
+            sähköinen asiointipalvelu ei kiirreellisten vikailmoituksten jättämistä varten.
+            </p></div>
+    </div>
+</div>
+    
+    <div class="row vali mx-0"></div>
+    <div class="row vali mx-0"></div>
+
+<div class="row kommentti3 text-center mx-0">
+    <h3>Tarjoamme ystävällistä ja asiakaslähtöistä palvelua, joka tuo lisäarvoa kiinteistöillesi.</h3>
+</div>
+ 
+
+    <div class="row vali mx-0"></div>
+    <div class="row vali mx-0"></div>
+
+<!--kontentti loppuu-->
 
 <?php include 'footer.php';?>
