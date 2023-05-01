783258d5eb073fe8ac7598f23eb343d5ea8b4f8b
AnttiK87 <Antti.Kortelainen@edu.lapinamk.fi>
2023-05-01 19:54:03 +0300

    Muokattu

--- Nettisivut/palvelut.php
+++ Nettisivut/palvelut.php
@@ -23,34 +23,34 @@
    
     <div class="row tyontekijat text-center mx-0">
         <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/repair2.png" class="img-fluid palvelut1 rounded-circle" alt="palvelut1"><br>
-        <p><br><b>Kiinteistönhuolto</b><br>
+        <p class="px-0 px-lg-5"><br><b>Kiinteistönhuolto</b><br>
             Yleiset ja ylläpitävät tehtävät, kuten kiinteistön kunnon jatkuva tarkkailu ja talotekniikan toiminnan seuranta. 
             Huolehdimme kiinteistöstäsi kokonaisvaltaisesti kausivaihtelut huomioivan suunnitelman mukaisesti, tietenkään 
             unohtamatta auto- ja roskakatoksia sekä muita yleisiätiloja.<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki12.jpg" class="img-fluid palvelut2 rounded-circle" alt="palvelut2">
-        <p><br><b>Lumityöt</b><br>
+        <p class="px-0 px-lg-5"><br><b>Lumityöt</b><br>
             Lumityöt hoituvat ketterästi uudella ja ympäristöystävällisellä kalustolla, joka on suunniteltu erityisesti ahtaita pihoja silmälläpitäen.
             Hoidamme myös liukkauden torjunnan, kattojen lumikuorman tarkkailun sekä tarvittaessa pihoihin kertyneiden aurauslumien poiskuljettamisen.<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki6.jpg" class="img-fluid palvelut3 rounded-circle" alt="palvelut3">
-        <p><br><b>Viheralueiden hoito</b><br>
+        <p class="px-0 px-lg-5"><br><b>Viheralueiden hoito</b><br>
             Tarjoamme puutarhuritasoista viheralueiden hoitoa. Huolehdimme niin piha-alueiden pensaista, puista, nurmikon leikkuusta, kulkuväylien siisteydestä 
             kuin syksyisestä lehtien haravoimisestakin. Tarvittaessa kauttamme hoituvat myös pensasaitojen leikkaukset sekä viheralueiden istutustyöt.<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki11.jpg" class="img-fluid palvelut4 rounded-circle" alt="palvelut4">
-        <p><br><b>Siivuostyöt</b><br>
+        <p class="px-0 px-lg-5"><br><b>Siivuostyöt</b><br>
             Huolehdimme siisteydestä tehokkaasti laadukkailla välineillä ja puhdistusaineilla asuinkiinteistöjen yleisissätiloissa sekä yritysten toimitiloissa. 
             Räätälöimme siivoispalvelut juuri teidän tarpeidenne ja toiveidenne mukaisesti. Hoidamme tarvittaessa myös ikkunoiden pesut sekä kertaluontoiset siivoukset 
             kuten asuntojen tyhjennykset.<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/repair1.jpg" class="img-fluid palvelut5 rounded-circle" alt="palvelut5">
-        <p><br><b>Remontit</b><br>
+        <p class="px-0 px-lg-5"><br><b>Remontit</b><br>
             Kauttamme onnistuvat myös yleisten- ja asuintilojenkin remontointi. Hoidamme niin pienet maalia ja tapettia kaipaavat pintaremontit kuin 
             suuremmat kaikenkattavat saneeraukset. Rakennusmiehemme ovat alansa ammattilaisia ja omaavat tarvittavat sertifikaatit laadukkaaseen ja luotettavaan korjaustöiden suorittamiseen.<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6 col-lg-4"><img src="img/palvelut/helsinki5.jpg" class="img-fluid palvelut6 rounded-circle" alt="palvelut6">
-        <p><br><b>24h päivystys</b><br>
+        <p class="px-0 px-lg-5"><br><b>24h päivystys</b><br>
             Vahingon sattuessa apu on lähellä. Huoltomiehemme päivystää kellonympäri vuoden jokaisena päivänä. Nyt verkkosivuillamme saatavilla on myös kellonajasta riippumaton 
             sähköinen asiointipalvelu ei kiirreellisten vikailmoituksten jättämistä varten.
             </p></div>
