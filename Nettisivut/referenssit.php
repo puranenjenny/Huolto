536ac983eb565e9f739c135f7beca7ffdbcf124f
AnttiK87 <Antti.Kortelainen@edu.lapinamk.fi>
2023-05-01 19:54:17 +0300

    Muokattu

--- Nettisivut/referenssit.php
+++ Nettisivut/referenssit.php
@@ -20,23 +20,23 @@
    
     <div class="row tyontekijat text-center mx-0">
         <div class="col-sm-12 col-md-6"><img src="img/palvelut/helsinki11.jpg" class="img-fluid palvelut1 rounded-circle" alt="palvelut1"><br>
-        <p class="px-5"><br><b>Ritu, asunto-osakeyhtiö Karhunpesä</b><br>
+        <p class="px-0 px-lg-5"><br><b>Ritu, asunto-osakeyhtiö Karhunpesä</b><br>
             "Tähän muutetaan teksi. Yleiset ja ylläpitävät tehtävät, kuten kiinteistön kunnon jatkuva tarkkailu ja talotekniikan toiminnan seuranta. 
             Huolehdimme kiinteistöstäsi kokonaisvaltaisesti kausivaihtelut huomioivan suunnitelman mukaisesti, tietenkään 
             unohtamatta auto- ja roskakatoksia sekä muita yleisiätiloja."<br>-Ritu-<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6"><img src="img/palvelut/helsinki6.jpg" class="img-fluid palvelut2 rounded-circle" alt="palvelut2">
-        <p class="px-5"><br><b>Hannu, omakotitalo isolla pihalla</b><br>
+        <p class="px-0 px-lg-5"><br><b>Hannu, omakotitalo isolla pihalla</b><br>
             "Tähän muutetaan teksi. Lumityöt hoituvat ketterästi uudella ja ympäristöystävällisellä kalustolla, joka on suunniteltu erityisesti ahtaita pihoja silmälläpitäen.
             Hoidamme myös liukkauden torjunnan, kattojen lumikuorman tarkkailun sekä tarvittaessa pihoihin kertyneiden aurauslumien poiskuljettamisen."<br>-Hannu-<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6"><img src="img/palvelut/helsinki5.jpg" class="img-fluid palvelut3 rounded-circle" alt="palvelut3">
-        <p class="px-5"><br><b>Tuula, toimistotilat Helsingin keskustassa</b><br>
+        <p class="px-0 px-lg-5"><br><b>Tuula, toimistotilat Helsingin keskustassa</b><br>
             "Tähän muutetaan teksi. Tarjoamme puutarhuritasoista viheralueiden hoitoa. Huolehdimme niin piha-alueiden pensaista, puista, nurmikon leikkuusta, kulkuväylien siisteydestä 
             kuin syksyisestä lehtien haravoimisestakin. Tarvittaessa kauttamme hoituvat myös pensasaitojen leikkaukset sekä viheralueiden istutustyöt."<br>-Tuula-<br><br>
             </p></div>
         <div class="col-sm-12 col-md-6"><img src="img/palvelut/helsinki4.jpg" class="img-fluid palvelut4 rounded-circle" alt="palvelut4">
-        <p class="px-5"><br><b>Petrus, Remonttikohde Viikissä</b><br>
+        <p class="px-0 px-lg-5"><br><b>Petrus, Remonttikohde Viikissä</b><br>
             "Tähän muutetaan teksi. Huolehdimme siisteydestä tehokkaasti laadukkailla välineillä ja puhdistusaineilla asuinkiinteistöjen yleisissätiloissa sekä yritysten toimitiloissa. 
             Räätälöimme siivoispalvelut juuri teidän tarpeidenne ja toiveidenne mukaisesti. Hoidamme tarvittaessa myös ikkunoiden pesut sekä kertaluontoiset siivoukset 
             kuten asuntojen tyhjennykset."<br>-Petrus-<br><br>
