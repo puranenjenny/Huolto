cce48ca03dd611434c856d235d46d25dc4c39f63
mirrmu <129037423+mirrmu@users.noreply.github.com>
2023-05-02 16:35:48 +0300

    https://github.com/puranenjenny/Huolto

---
+++ Nettisivut/php/tyotehtavalistaus.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+
+include("config.php"); 
+
+$kysely = "SELECT tehtava_id, kuvaus, korjaustoimenpide, tila, tyyppi, tila_id, yleisavaimen_kaytto, tehtavan_tyyppi_id, tyontekija_id, tehtavan_tilanne_id FROM tehtavat"; 
+$data = $yhteys->query($kysely); 
+
+$JSON = '{"tehtavat":['; 
+$laskuri = 0; 
+$riveja = $data->rowCount(); 
+
+while($rivi = $data->fetch(PDO::FETCH_ASSOC)){ 
+    $laskuri++; 
+    $JSON.= '{"Tehtava_id":"' .$rivi['tehtava_id'] . '", "Kuvaus":"' . $rivi['kuvaus'] . '", "Korjaustoimenpide":"' . $rivi['korjaustoimenpide'] . '", "tila":"' . $rivi['tila'] . '", "Tyyppi":"' . $rivi['tyyppi'] . '", "tila_id":"' . $rivi['tila_id'] .'", "yleisavaimen_kaytto":"' . $rivi['yleisavaimen_kaytto'] .'", "tehtavan_tyyppi_id":"' . $rivi['tehtavan_tyyppi_id'] .'", "tyontekija_id":"' . $rivi['tyontekija_id'] .'", "tehtavan_tilanne_id":"' . $rivi['tehtavan_tilanne_id'] .'"}';
+    if($laskuri < $riveja) $JSON.= ","; 
+}
+
+$JSON.= ']}'; 
+
+$yhteys = null; 
+
+$handler = fopen("tyotehtavat.json", "w"); 
+fwrite($handler, $JSON);
+fclose($handler);
+
+
+
+
+?>
