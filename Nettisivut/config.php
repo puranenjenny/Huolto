3f9855e86c30c64ff9f03eea207a33205c6ee19f
mirrmu <129037423+mirrmu@users.noreply.github.com>
2023-05-02 14:17:25 +0300

    https://github.com/puranenjenny/Huolto

--- Nettisivut/config.php
+++ Nettisivut/config.php
@@ -7,7 +7,7 @@ $tietokanta = "kiinteistohuolto2";
 $tunnus = "root";
 $salasana = "";
 
-$yhteys = new PDO("mysql:host=$palvelin; dbname=$kiinteistohuolto2", $tunnus, $salasana);
+$yhteys = new PDO("mysql:host=$palvelin; dbname=$tietokanta", $tunnus, $salasana);
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch(PDOExpeption $e){
