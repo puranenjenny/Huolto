0ef5d6b48e3e90ad0bd8607af0797203890f6623
mirrmu <129037423+mirrmu@users.noreply.github.com>
2023-05-04 14:13:42 +0300

    muutettu koodit kirjautumaan rooli_id:n mukaan

--- Nettisivut/php/asukas_kirjautumiskoodi.php
+++ Nettisivut/php/asukas_kirjautumiskoodi.php
@@ -10,22 +10,29 @@ if(!isset($_SESSION)){  //jos session ei ole käynnissä, käynnistä se
       $tunnus = $_POST['tunnus'];
       $salasana = $_POST['salasana']; 
       
-      $kirjaudu = $yhteys->prepare("SELECT kayttaja_id FROM kayttajat WHERE tunnus = :tunnus and salasana = :salasana");
+      $kirjaudu = $yhteys->prepare("SELECT kayttaja_id, rooli_id FROM kayttajat WHERE tunnus = :tunnus and salasana = :salasana");
       $kirjaudu->bindParam(':tunnus', $tunnus);
       $kirjaudu->bindParam(':salasana', $salasana);
       $kirjaudu->execute();
-      
-      $count = $kirjaudu->rowCount();
-		
-      if($count == 1) {
-         $_SESSION['login_user'] = $tunnus;
-         header("location: ../vikalomake.php"); //siirrytään vikailmoitukseen
-      }else {
-         $_SESSION['error'] = "<b>Kirjautuminen ei onnistunut!<br> 
-         Tarvittaessa ota yhteys toimistoomme toimisto@rautio.fi</b>"; //errorviesti jos kirjautuminen ei onnistu
-         header("location: ../kirjaudu_asukas.php");
-         
-      }
+
+$count = $kirjaudu->rowCount();
+
+if($count == 1) {
+   $kayttaja = $kirjaudu->fetch();
+   if($kayttaja['rooli_id'] == '4') { // 1=toimistohenkilo, 2=huoltohenkilo, 3=isannoitsija, 4=asukas
+      $_SESSION['login_user'] = $tunnus;
+      header("location: ../vikalomake.php"); //siirrytään vikailmoitukseen
+   } else {
+      $_SESSION['error'] = "<b>Kirjautuminen ei onnistunut!<br> 
+      Tarvittaessa ota yhteys toimistoomme toimisto@rautio.fi</b>"; //errorviesti jos kirjautuminen ei onnistu
+      header("location: ../kirjaudu_asukas.php");
    }
+}else {
+   $_SESSION['error'] = "<b>Kirjautuminen ei onnistunut!<br> 
+   Tarvittaessa ota yhteys toimistoomme toimisto@rautio.fi</b>"; //errorviesti jos kirjautuminen ei onnistu
+   header("location: ../kirjaudu_asukas.php");
+}
+      }
+   
 
 ?>
--- Nettisivut/php/tyontekija_kirjautumiskoodi.php
+++ Nettisivut/php/tyontekija_kirjautumiskoodi.php
@@ -2,7 +2,7 @@
 include("config.php");
 
 if(!isset($_SESSION)){  //jos session ei ole käynnissä, käynnistä se
-session_start(); 
+  session_start(); 
 }
 
    if($_SERVER["REQUEST_METHOD"] == "POST") { 
@@ -10,23 +10,29 @@ session_start();
       $tunnus = $_POST['tunnus'];
       $salasana = $_POST['salasana']; 
       
-      $kirjaudu = $yhteys->prepare("SELECT kayttaja_id FROM kayttajat WHERE tunnus = :tunnus and salasana = :salasana");
+      $kirjaudu = $yhteys->prepare("SELECT kayttaja_id, rooli_id FROM kayttajat WHERE tunnus = :tunnus and salasana = :salasana");
       $kirjaudu->bindParam(':tunnus', $tunnus);
       $kirjaudu->bindParam(':salasana', $salasana);
       $kirjaudu->execute();
-      
-      $count = $kirjaudu->rowCount();
-		
-      if($count == 1) {
-         $_SESSION['login_user'] = $tunnus;
-         header("location: ../ui_toimisto.php"); //siirrytään työpöydälle
-        //Tähän lisättävä ehtoja ja ohjaus sen mukaan onko toimistotyöntekijä vai huoltohenkilö
 
-      }else {
-         $_SESSION['error'] = "<b>Kirjautuminen ei onnistunut!<br> 
-         Tarvittaessa ota yhteys toimistoomme toimisto@rautio.fi</b>"; //errorviesti jos kirjautuminen ei onnistu
-         header("location: ../kirjaudu_asukas.php");
-      }
+$count = $kirjaudu->rowCount();
+
+if($count == 1) {
+   $kayttaja = $kirjaudu->fetch();
+   if($kayttaja['rooli_id'] == '2') { // 1=toimistohenkilo, 2=huoltohenkilo, 3=isannoitsija, 4=asukas
+      $_SESSION['login_user'] = $tunnus;
+      header("location: ../vikalomake.php"); //siirrytään vikailmoitukseen
+   } else {
+      $_SESSION['error'] = "<b>Kirjautuminen ei onnistunut!<br> 
+      Tarvittaessa ota yhteys toimistoomme toimisto@rautio.fi</b>"; //errorviesti jos kirjautuminen ei onnistu
+      header("location: ../kirjaudu_asukas.php");
    }
+}else {
+   $_SESSION['error'] = "<b>Kirjautuminen ei onnistunut!<br> 
+   Tarvittaessa ota yhteys toimistoomme toimisto@rautio.fi</b>"; //errorviesti jos kirjautuminen ei onnistu
+   header("location: ../kirjaudu_asukas.php");
+}
+      }
+   
 
 ?>
