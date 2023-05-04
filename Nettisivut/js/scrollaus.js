769005f679afe63ef10af0029aefcc4bae7123a0
puranenjenny <puranenjenny@gmail.com>
2023-05-04 10:04:08 +0300

    scrollaus säätöä

--- Nettisivut/js/scrollaus.js
+++ Nettisivut/js/scrollaus.js
@@ -1,11 +1,4 @@
-//pitäisi scrollata takas ylös aina ku refreshaa sivun mutta ei tee sitä
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     window.scrollTo(0, 0);
-//   });
-
-window.addEventListener("load", function () {
-    setTimeout(function () {
-      window.scrollTo(0, 0);
-    }, 0);
-  });
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// };
