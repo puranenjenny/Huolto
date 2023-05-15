3e0158ab586a2d95663c34702c196d1aa2c0bcdf
puranenjenny <puranenjenny@gmail.com>
2023-05-15 13:24:26 +0300

    kommentit lisaa asukas

--- Nettisivut/lisaa_asukas.php
+++ Nettisivut/lisaa_asukas.php
@@ -1,7 +1,7 @@
 <?php 
-session_start();
-include "php/config.php"; 
-include 'header_ui_toimisto.php';?>
+session_start(); //käynnistetään sessio
+include "php/config.php"; //yhteys tietokantaan
+include 'header_ui_toimisto.php';?> <!-- toimiston header -->
 
 <head>
   <title>Asukkaan lisäys</title>
@@ -10,14 +10,15 @@ include 'header_ui_toimisto.php';?>
 <body>
   <div class="text-center container4 center">
     <?php
-    try {
-      if (isset($_POST['submit'])) {
-        // Otetaan lomakkeelta saadut tiedot talteen
+    try { //yritetään ottaa yhteys tietokantaan
+      if (isset($_POST['submit'])) { //jos painetaan submit nappia
+
+        // Otetaan lomakkeelta saadut tiedot talteen muuttujiin
         $etunimi = $_POST['etunimi'];//asukkaat taulu
         $sukunimi = $_POST['sukunimi'];//asukkaat taulu
         $tunnus = $_POST['tunnus'];//käyttäjät taulu
         $salasana = $_POST['salasana'];//käyttäjät taulu
-        $kayttaja = "Asukas";//käyttäjät taulu
+        $kayttaja = "Asukas";//käyttäjät taulu (ei lomakkeelta, vaan aina sama arvo)
         $taloyhtio = $_POST['taloyhtio'];//asukkaat taulu
         $rappu = $_POST['rappu'];//asukkaat taulu
 
@@ -26,7 +27,7 @@ include 'header_ui_toimisto.php';?>
 
         //HOX! Lisäysjärjestyksellä väliä, että toimii koodi. 
 
-         // Lisätään uusi käyttäjä tietokantaan
+         // Lisätään uusi käyttäjä tietokantaan kayttajat taululle
          $lisaa_kayttaja = $yhteys->prepare("INSERT INTO kayttajat (tunnus, salasana, kayttaja, rooli_id) VALUES (?, ?, ?, ?)");
          $kryptattu_salasana = password_hash($salasana, PASSWORD_DEFAULT); //kryptataan salasana
          $lisaa_kayttaja->execute([$tunnus, $kryptattu_salasana, $kayttaja, 4]);
@@ -34,7 +35,7 @@ include 'header_ui_toimisto.php';?>
          // Haetaan uuden käyttäjän id
          $kayttaja_id = $yhteys->query("SELECT LAST_INSERT_ID()")->fetchColumn();
 
-        // Lisätään uusi asukas tietokantaan
+        // Lisätään uusi asukas tietokantaan asukkaat taululle hyödyntäen käyttäjän id:tä
         $lisaa_asukas = $yhteys->prepare("INSERT INTO asukkaat (etunimi, sukunimi, kayttaja_id, taloyhtio_id, rappu) VALUES (?, ?, ?, ?, ?)");
         $lisaa_asukas->execute([$etunimi, $sukunimi, $kayttaja_id, $taloyhtio, $rappu]);
 
@@ -48,8 +49,8 @@ include 'header_ui_toimisto.php';?>
         // Näytetään onnistumisviesti
         echo "<h3>Asukkaan lisääminen onnistui!</h3>";
 
-      } else {
-        echo "Lomaketta ei ole lähetetty.";
+      } else { 
+        echo "Lomaketta ei ole lähetetty."; 
       }
     } catch(PDOException $e) {
         // Jos tulee virhe, perutaan transaktio ja näytetään virheilmoitus
@@ -62,4 +63,4 @@ include 'header_ui_toimisto.php';?>
     ?>
   </div>
 </body>
-    <?php include "footer.php"; ?>
+    <?php include "footer.php"; ?> <!-- footer -->
