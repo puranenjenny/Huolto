086d129178d8f9c277f675fc23f018e49009e320
puranenjenny <puranenjenny@gmail.com>
2023-05-17 09:31:32 +0300

    poistettu turhat kuvat

--- Nettisivut/js/isannoitsija_valinnat.js
+++ Nettisivut/js/isannoitsija_valinnat.js
@@ -1,14 +1,14 @@
 // tämä toiminto näyttää vain valitun taloyhtiön tilat
-$(document).ready(function() { 
-  $('#taloyhtio').on('change', function() {
-      var taloyhtio_id = $(this).val();
-      $('#tila option').each(function() {
-          if ($(this).data('taloyhtio-id') == taloyhtio_id || $(this).val() == '') {
-              $(this).show();
+$(document).ready(function() { // kun sivu on ladattu
+  $('#taloyhtio').on('change', function() { // kun taloyhtiö valinta muuttuu
+      var taloyhtio_id = $(this).val(); // haetaan valitun taloyhtiön id
+      $('#tila option').each(function() { // käydään läpi kaikki tila valinnat
+          if ($(this).data('taloyhtio-id') == taloyhtio_id || $(this).val() == '') { // jos taloyhtiö id on sama kuin valitun taloyhtiön id tai valinta on tyhjä
+              $(this).show(); // näytetään valinta
           } else {
-              $(this).hide();
+              $(this).hide(); // muuten piilotetaan valinta
           }
       });
-      $('#tila').val('');
+      $('#tila').val(''); // tyhjennetään valinta
   });
 });
