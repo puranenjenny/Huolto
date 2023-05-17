444a76373f2e0afa058bc0fa812f1cb0eb1f69f8
AnttiK87 <Antti.Kortelainen@edu.lapinamk.fi>
2023-05-17 22:15:41 +0300

    tehty muutoksia taloyhtiön poistoon

--- Nettisivut/php/poistaTaloyhtio.php
+++ Nettisivut/php/poistaTaloyhtio.php
@@ -3,6 +3,17 @@ require "config.php";
 
 if(isset($_GET['id'])){
     $taloyhtio_id = $_GET['id'];
+
+    $query = "DELETE FROM tehtavat WHERE taloyhtio_id = :taloyhtio_id";
+    $delete = $yhteys->prepare($query);
+    $delete->bindValue(':taloyhtio_id', $taloyhtio_id, PDO::PARAM_STR);
+    $delete->execute();
+
+    $query = "DELETE FROM tilat WHERE taloyhtio_id = :taloyhtio_id";
+    $delete = $yhteys->prepare($query);
+    $delete->bindValue(':taloyhtio_id', $taloyhtio_id, PDO::PARAM_STR);
+    $delete->execute();
+
     $query = "DELETE FROM taloyhtiot WHERE taloyhtio_id = :taloyhtio_id";
     $delete = $yhteys->prepare($query);
     $delete->bindValue(':taloyhtio_id', $taloyhtio_id, PDO::PARAM_STR);
